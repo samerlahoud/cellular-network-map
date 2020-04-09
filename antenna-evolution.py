e673ccd50c8ac7ebc03d7de553c1bd17abac7b72
samerlahoud <samer@lahoud.fr>
2020-04-10 00:47:45 +0300

    font type and size

--- antenna-evolution.py
+++ antenna-evolution.py
@@ -1,10 +1,13 @@
 # Get the json file from data.anfr.fr
 # Two filter in use: postal code and activation
 import sys
-import json
-import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = 'CMU Serif'
+plt.rcParams['axes.labelsize'] = 14
+#plt.rcParams.update({'font.size': 12})
 
 # Read .csv and parse date column
 antennas = pd.read_csv('./data/cartoradio-35000/7a2457be-a5f1-4075-8481-4515fc90acc0.csv', sep=';', \
@@ -20,8 +23,8 @@ antennas['year'] = antennas.index.strftime('%Y')
 cumul_antennas = antennas.groupby(['month','emr_lb_systeme']).size().unstack().fillna(0).cumsum()
 perc_antennas = cumul_antennas.div(cumul_antennas.sum(axis=1), axis=0)
 
-fig, ax = plt.subplots()
 n=10
+fig, ax = plt.subplots()
 cumul_antennas.plot(kind='bar',stacked=True, ax=ax)
 ticks = ax.xaxis.get_ticklocs()
 ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
@@ -31,13 +34,17 @@ ax.set_xlabel("Date of deployment")
 ax.set_ylabel("Cumulated number of antennas")
 plt.tight_layout()
 ax.get_legend().set_title('Frequency band')
-fig.savefig('cum_antenna_evo.pdf')
+fig.savefig('./output/cum_antenna_evo.pdf', format='pdf', bbox_inches='tight')
 
 fig, ax = plt.subplots()
-perc_antennas.plot(kind='area',stacked=True, ax=ax)
+perc_antennas.plot(kind='bar',stacked=True, ax=ax)
+ticks = ax.xaxis.get_ticklocs()
+ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
+ax.xaxis.set_ticks(ticks[::n])
+ax.xaxis.set_ticklabels(ticklabels[::n])
 ax.set_xlabel("Date of deployment")
 ax.set_ylabel("Percentage of antennas")
 plt.xticks(rotation=90)
 plt.tight_layout()
 ax.get_legend().set_title('Frequency band')
-fig.savefig('perc_antenna_evo.pdf')
+fig.savefig('./output/perc_antenna_evo.pdf', format='pdf', bbox_inches='tight')
