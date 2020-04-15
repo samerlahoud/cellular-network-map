78920a86af1c674e4199543de11e76baf12e323c
samerlahoud <samer@lahoud.fr>
2020-04-15 23:32:03 +0300

    stable version

--- freq-reuse-data-aggregate.py
+++ freq-reuse-data-aggregate.py
@@ -2,6 +2,7 @@
 # Adapted for cities with mutiple postal codes
 import sys
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'serif'
@@ -20,45 +21,30 @@ antennas = pd.read_csv(base_folder+'data-anfr-rennes-lsquare-active.csv', sep=';
     index_col=['emr_dt_service'], parse_dates=['emr_dt_service'])
 
 # Keep only the city antennas
-antennas = antennas[antennas.sup_id.isin(support_id.values)]
+# set gives unique values
+antennas = antennas[antennas.sup_id.isin(set(support_id.values))]
 
 # Get frequency bands
 # Remove french accents and spaces from column names
 freq_bands = pd.read_csv(base_folder+'cartoradio-rennes/Antennes_Emetteurs_Bandes_Cartoradio.csv', sep=';')
-freq_bands = freq_bands[freq_bands.Numero_de_support.isin(antennas.sup_id.values)]
+freq_bands = freq_bands[freq_bands.Numero_de_support.isin(set(antennas.sup_id.values))]
+freq_bands = freq_bands[freq_bands.Systeme.isin(set(antennas.emr_lb_systeme))]
 
-# Add year and month column
-antennas['month'] = antennas.index.strftime('%Y-%m')
-antennas['year'] = antennas.index.strftime('%Y')
+freq_range = np.arange(min(freq_bands.Debut),max(freq_bands.Fin), 0.1)
+freq_df = pd.DataFrame(index=freq_range)
+freq_df['freq_use'] = 0
 
-# Group by month then by emr_lb_systeme (technology)
-# Put in a a matrix (month, emr_lb_systeme) with unstack 
-# Replace nan with zero then compute a cumulative sum
-cumul_antennas = antennas.groupby(['month','emr_lb_systeme']).size().unstack().fillna(0).cumsum()
-perc_antennas = cumul_antennas.div(cumul_antennas.sum(axis=1), axis=0)
+for index, row in freq_bands.iterrows():
+    freq_df.loc[row['Debut']:row['Fin'], :] +=1
 
-n=10
+n=500
 fig, ax = plt.subplots()
-cumul_antennas.plot(kind='bar',stacked=True, ax=ax)
+freq_df.plot(kind='bar', ax=ax)
 ticks = ax.xaxis.get_ticklocs()
 ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
 ax.xaxis.set_ticks(ticks[::n])
 ax.xaxis.set_ticklabels(ticklabels[::n])
-ax.set_xlabel("Date of deployment")
-ax.set_ylabel("Cumulated number of antennas")
+ax.set_xlabel("Frequency band")
+ax.set_ylabel("Number of antennas")
 plt.tight_layout()
-ax.get_legend().set_title('Frequency band')
-fig.savefig('./output/cum_antenna_evo.pdf', format='pdf', bbox_inches='tight')
-
-fig, ax = plt.subplots()
-perc_antennas.plot(kind='bar',stacked=True, ax=ax)
-ticks = ax.xaxis.get_ticklocs()
-ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
-ax.xaxis.set_ticks(ticks[::n])
-ax.xaxis.set_ticklabels(ticklabels[::n])
-ax.set_xlabel("Date of deployment")
-ax.set_ylabel("Percentage of antennas")
-plt.xticks(rotation=90)
-plt.tight_layout()
-ax.get_legend().set_title('Frequency band')
-fig.savefig('./output/perc_antenna_evo.pdf', format='pdf', bbox_inches='tight')
+fig.savefig('./output/freq_reuse.pdf', format='pdf', bbox_inches='tight')
