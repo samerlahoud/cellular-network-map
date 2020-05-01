78cab9fcfc525a751749e5fb93b12ce324a7f5d3
samerlahoud <samer@lahoud.fr>
2020-05-01 21:08:59 +0300

    Update antenna-evolution-data-aggregate.py

--- antenna-evolution-data-aggregate.py
+++ antenna-evolution-data-aggregate.py
@@ -25,12 +25,15 @@ antennas = antennas[antennas.sup_id.isin(support_id.values)]
 # Add year and month column
 antennas['month'] = antennas.index.strftime('%Y-%m')
 antennas['year'] = antennas.index.strftime('%Y')
+antennas['freq_band'] = antennas.emr_lb_systeme.str.split(expand=True)[1]
 
 # Group by month then by emr_lb_systeme (technology)
 # Put in a a matrix (month, emr_lb_systeme) with unstack 
 # Replace nan with zero then compute a cumulative sum
 cumul_antennas = antennas.groupby(['month','emr_lb_systeme']).size().unstack().fillna(0).cumsum()
+cumul_band = antennas.groupby(['month','freq_band']).size().unstack().fillna(0).cumsum()
 perc_antennas = cumul_antennas.div(cumul_antennas.sum(axis=1), axis=0)
+perc_band = cumul_band.div(cumul_band.sum(axis=1), axis=0)
 
 n=10
 fig, ax = plt.subplots()
@@ -59,3 +62,16 @@ plt.legend(bbox_to_anchor=(1.3, 1.0), loc='upper right')
 #plt.tight_layout()
 ax.get_legend().set_title('Frequency band')
 fig.savefig('./output/perc_antenna_evo.pdf', format='pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots()
+perc_band.plot(kind='bar',stacked=True, ax=ax)
+ticks = ax.xaxis.get_ticklocs()
+ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
+ax.xaxis.set_ticks(ticks[::n])
+ax.xaxis.set_ticklabels(ticklabels[::n])
+ax.set_xlabel("Date of deployment")
+ax.set_ylabel("Percentage of antennas")
+plt.legend(bbox_to_anchor=(1.3, 1.0), loc='upper right')
+#plt.tight_layout()
+ax.get_legend().set_title('Frequency band')
+fig.savefig('./output/perc_band_evo.pdf', format='pdf', bbox_inches='tight')
